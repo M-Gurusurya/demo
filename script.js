b84ba8bb7c563f746c01975386e352642b246bc2
Gurusurya M <118505440+M-Gurusurya@users.noreply.github.com>
2023-12-18 20:26:48 +0530

    script.js

--- script.js
+++ script.js
@@ -3,23 +3,20 @@ function updateTime() {
     const currentTime = now.toLocaleTimeString();
     document.getElementById('current-time').textContent = currentTime;
   }
-  
-  // Update time every second
   setInterval(updateTime, 1000);
   
-  // Initial call to display time immediately
   updateTime();
   
   const busScheduleFile = 'busTimings.json';
   let busData;
-fetch(busScheduleFile)
-  .then(response => response.json())
-  .then(data => {
-    busData = data;
-  })
-  .catch(error => {
-    console.error('Error fetching bus schedule:', error);
-  });
+    fetch(busScheduleFile)
+    .then(response => response.json())
+    .then(data => {
+        busData = data;
+    })
+    .catch(error => {
+        console.error('Error fetching bus schedule:', error);
+    });
 
 // Function to get the next three buses
 function getNextBuses() {
@@ -31,11 +28,36 @@ function getNextBuses() {
     return;
   }
 
-  const currentTime = new Date().getTime();
+  const currentTime = new Date(); // Get current time
+  const currentHours = currentTime.getHours();
+  const currentMinutes = currentTime.getMinutes();
+  const currentAMPM = (currentHours >= 12) ? "PM" : "AM";
+
   const nextBuses = busData
     .filter(bus => bus.from_location === fromLocation && bus.to_location === toLocation)
     .sort((a, b) => new Date(`01/01/2023 ${a.time}`) - new Date(`01/01/2023 ${b.time}`))
-    .filter(bus => new Date(`01/01/2023 ${bus.time}`).getTime() > currentTime)
+    .filter(item => {
+      // Extract hours, minutes, and AM/PM from the time string
+      const [time, ampm] = item.time.split(' ');
+      const [hours, minutes] = time.split(':').map(num => parseInt(num));
+  
+      // Convert timings to 24-hour format for comparison
+      let hours24 = hours;
+      if (ampm === "PM" && hours !== 12) {
+        hours24 += 12;
+      } else if (ampm === "AM" && hours === 12) {
+        hours24 = 0;
+      }
+  
+      // Compare timings
+      if (
+        (hours24 > currentHours || (hours24 === currentHours && minutes > currentMinutes)) &&
+        ampm === currentAMPM
+      ) {
+        return true;
+      }
+      return false;
+    })
     .slice(0, 3);
 
     // Result
@@ -78,7 +100,61 @@ function getNextBuses() {
     busInfoElement.appendChild(table);
 }
 
-// Event listener for the button click
 const showBusesButton = document.getElementById('showBuses');
 showBusesButton.addEventListener('click', getNextBuses);
+
+function getAllBuses() {
+  const fromLocation = document.getElementById('from').value;
+  const toLocation = document.getElementById('to').value;
+
+  if (!busData) {
+    console.error('Bus data not available');
+    return;
+  }
+
+  const nextBuses = busData
+    .filter(bus => bus.from_location === fromLocation && bus.to_location === toLocation)
+    .sort((a, b) => new Date(`01/01/2023 ${a.time}`) - new Date(`01/01/2023 ${b.time}`))
+
+    // Result
+    const busInfoElement = document.getElementById('busInfo');
+    busInfoElement.innerHTML = '';
+    
+    if (nextBuses.length === 0) {
+      busInfoElement.textContent = 'No upcoming buses found.';
+      return;
+    }
+    
+    const table = document.createElement('table');
+    const tableHeader = document.createElement('tr');
+    ['Sno', 'Time', 'Bus Name'].forEach(headerText => {
+      const th = document.createElement('th');
+      th.textContent = headerText;
+      tableHeader.appendChild(th);
+    });
+    table.appendChild(tableHeader);
+    
+    nextBuses.forEach((bus, index) => {
+      const row = document.createElement('tr');
+      
+      const snoCell = document.createElement('td');
+      snoCell.textContent = index + 1;
+      row.appendChild(snoCell);
+    
+      const timeCell = document.createElement('td');
+      timeCell.textContent = bus.time;
+      row.appendChild(timeCell);
+    
+      const busNameCell = document.createElement('td');
+      busNameCell.textContent = bus.bus_name;
+      row.appendChild(busNameCell);
+    
+      table.appendChild(row);
+    });
+    
+    busInfoElement.appendChild(table);
+}
+
+const showAllBusesButton = document.getElementById('showAll');
+showAllBusesButton.addEventListener('click', getAllBuses);
 
